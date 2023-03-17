14f78f2b23ea6ecb926e8bf6ab059abb1f8a15c7
marciomaia2020 <marciofernando.maia@yahoo.com.br>
2023-03-17 17:18:54 -0300

    Update files

---
+++ script.js
@@ -0,0 +1,16 @@
+var A = 10;
+var B = 2;
+var C = 10
+var soma = A + B + C;
+var somarApenas = A + C;
+console.log(soma);
+console.log(somarApenas);
+
+/*
+
+if((5 == (10-5)) && (10 == (5 + 5))){
+    System.out.println(“Estas expressões são ambas verdadeiras”);
+    }
+
+    */
+    
